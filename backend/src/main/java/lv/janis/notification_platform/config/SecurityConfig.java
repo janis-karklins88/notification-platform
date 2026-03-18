334ff53915cb41b865733f6e72aa6c3ff378a0e5
Janis Karklins <janis.karklins88@gmail.com>
2026-03-18 12:19:52 +0200

    clear up

--- backend/src/main/java/lv/janis/notification_platform/config/SecurityConfig.java
+++ backend/src/main/java/lv/janis/notification_platform/config/SecurityConfig.java
@@ -9,7 +9,6 @@ import java.util.Set;
 
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
-import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.convert.converter.Converter;
